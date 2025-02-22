68c248a12b1c397247cd0440c41b454baa7ba2a5
Luis Gómez Alonso <luis.gomez.alonso95@gmail.com>
2025-02-22 11:12:58 +0100

    fix black lint & better CI triggers

--- api/ci/kpi/models.py
+++ api/ci/kpi/models.py
@@ -4,9 +4,11 @@ import json
 
 CI_KPI_RELATIVE_PATH = "././model/json_schema/ci_testing.json"
 
+
 class ADDONS(AuxBaseModel):
     version: str
     name: str
+
 
 class CI_KPI(AuxBaseModel):
     startDate: int
@@ -21,11 +23,11 @@ class CI_KPI(AuxBaseModel):
     context: dict
     result: Union[bool, dict, str, int]
     addons: list[ADDONS] = Field(default_factory=lambda: [])
+
     @computed_field
     @property
     def duration(self) -> int:
         return self.endDate - self.startDate
-
 
 
 with open(CI_KPI_RELATIVE_PATH) as f:
